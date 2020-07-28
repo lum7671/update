4f3dd6d3df6b509885fd929e0db04e1c785abd27
장두현(Doohyun Jang)/Data Engineering팀/SKP <nate.jang@sk.com>
2020-07-28 13:43:56 +0900

    cask 제거

--- config/update/update-run-last/do-last.sh
+++ config/update/update-run-last/do-last.sh
@@ -6,21 +6,6 @@ brew cu -ay --cleanup
 brew cleanup -s
 rm -rf $(brew --cache)
 
-
-# cask
-if [ -f "$HOME/Library/Preferences/Aquamacs Emacs/Packages/Cask" ]; then
-	(cd "$HOME/Library/Preferences/Aquamacs Emacs/Packages";cask upgrade-cask;cask install;cask update;cask clean-elc;cask build)
-	/usr/local/bin/emacs --batch --eval '(byte-recompile-directory "~/Library/Preferences/Aquamacs Emacs/Packages")'
-	sync
-fi
-
-if [ -f "$HOME/.emacs.d/Cask" ]; then
-	(cd "$HOME/.emacs.d";cask upgrade-cask;cask install;cask update;cask clean-elc;cask build)
-	/usr/local/bin/emacs --batch --eval '(byte-recompile-directory "~/.emacs.d")'
-	sync
-fi
-
-
 ### Added by Zplugin's installer
 source '/Users/x/.zinit/bin/zinit.zsh'
 autoload -Uz _zinit
@@ -28,7 +13,6 @@ autoload -Uz _zinit
 
 zinit self-update
 zinit update
-
 
 # kymsu
 kymsu cleanup
