a8807979140bd271703085ace6f0b926cf00e57e
장두현(Doohyun Jang)/Data Engineering팀/SKP <nate.jang@sk.com>
2020-08-04 18:48:14 +0900

    cask 다시 살림.

--- config/update/update-run-last/do-last.sh
+++ config/update/update-run-last/do-last.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env zsh -li
 
-
 # brew cleanup
 brew cu -ay --cleanup
 brew cleanup -s
 rm -rf $(brew --cache)
+
+# cask
+if [ -f "$HOME/Library/Preferences/Aquamacs Emacs/Packages/Cask" ]; then
+	(cd "$HOME/Library/Preferences/Aquamacs Emacs/Packages";cask upgrade-cask;cask install;cask update;cask clean-elc;cask build)
+	/usr/local/bin/emacs --batch --eval '(byte-recompile-directory "~/Library/Preferences/Aquamacs Emacs/Packages")'
+	sync
+fi
+
+if [ -f "$HOME/.emacs.d/Cask" ]; then
+	(cd "$HOME/.emacs.d";cask upgrade-cask;cask install;cask update;cask clean-elc;cask build)
+	/usr/local/bin/emacs --batch --eval '(byte-recompile-directory "~/.emacs.d")'
+	sync
+fi
 
 ### Added by Zplugin's installer
 source '/Users/x/.zinit/bin/zinit.zsh'
