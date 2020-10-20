f7ec30dd7ec855360b6259cd48b0abcca5a3144f
장두현(Doohyun Jang)/Data Engineering팀/SKP <nate.jang@sk.com>
2020-10-20 11:16:20 +0900

    설치 순서 변경.

--- config/update/update-run-first/nvm/update.sh
+++ config/update/update-run-first/nvm/update.sh
@@ -5,6 +5,6 @@
 ) && \. "$NVM_DIR/nvm.sh"
 
 nvm install 'lts/*' --reinstall-packages-from=current
-nvm install node --reinstall-packages-from=node
 nvm install lts/* --reinstall-packages-from=default --latest-npm
 nvm install iojs --reinstall-packages-from=iojs
+nvm install node --reinstall-packages-from=node
