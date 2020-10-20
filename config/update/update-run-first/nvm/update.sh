638a4dd3b7483ff47bb69cf51b7e2385a22cb872
장두현(Doohyun Jang)/Data Engineering팀/SKP <nate.jang@sk.com>
2020-10-20 12:53:40 +0900

    unstable 추가.

--- config/update/update-run-first/nvm/update.sh
+++ config/update/update-run-first/nvm/update.sh
@@ -6,5 +6,6 @@
 
 nvm install 'lts/*' --reinstall-packages-from=current
 nvm install lts/* --reinstall-packages-from=default --latest-npm
+nvm install unstable --reinstall-packages-from=unstable
 nvm install iojs --reinstall-packages-from=iojs
 nvm install node --reinstall-packages-from=node
